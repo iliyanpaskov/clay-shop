70d590e3c0620d7c64e393cf5a3e487bfb1a298c
iliyanpaskov <108199866+iliyanpaskov@users.noreply.github.com>
2023-11-09 22:19:39 +0200

    CHeart component created

--- src/components/CurrentProduct/CurrentProduct.jsx
+++ src/components/CurrentProduct/CurrentProduct.jsx
@@ -1,4 +1,5 @@
 import { MainImage } from '../MainImage/MainImage';
+import { CHeart } from '../common/CHeart/CHeart';
 import { CImageConrainer } from '../common/CImageConrainer/CImageConrainer';
 import { CPopularity } from '../common/CPopularity/CPopularity';
 import { CTransparentEffect } from '../common/CTransparentEffect/CTransparentEffect';
@@ -25,6 +26,7 @@ export const CurrentProduct = ({
             <article className='current__product__information'>
                 <section className='current__product__information__popularity' >
                     <CPopularity popularity={product.popularity}/>
+                    <CHeart/>
                 </section>
             </article>
         </section>
---
+++ src/components/common/CHeart/CHeart.jsx
@@ -0,0 +1,10 @@
+import {BsSuitHeart} from 'react-icons/bs';
+import './CHeart.scss';
+
+export const CHeart = () => {
+  return (
+    <div className='popularity__heart'>
+        <BsSuitHeart/>
+    </div>
+  )
+}
