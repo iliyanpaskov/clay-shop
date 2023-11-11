d87a47863f89c982da37233cbd157ac55256fa2b
iliyanpaskov <108199866+iliyanpaskov@users.noreply.github.com>
2023-11-11 16:14:57 +0200

    CLoader component created

---
+++ src/components/common/CLoader/CLoader.jsx
@@ -0,0 +1,11 @@
+import './CLoader.scss';
+
+export const CLoader = () => {
+    return (
+        <div className="loader">
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+    )
+}
--- src/pages/ProductPage/ProductPage.jsx
+++ src/pages/ProductPage/ProductPage.jsx
@@ -4,6 +4,7 @@ import { CurrentProduct } from '../../components/CurrentProduct/CurrentProduct';
 import { CurrentProductReview } from '../../components/CurrentProductReview/CurrentProductReview';
 import { CommentsContext } from '../../context/CommentsContext';
 import './ProductPage.scss';
+import { CLoader } from '../../components/common/CLoader/CLoader';
 
 export const ProductPage = () => {
 
@@ -23,12 +24,18 @@ export const ProductPage = () => {
     return (
 
         <section className='product__page'>
-            <article className='product__page__current__product'>
-                <CurrentProduct product={currentProduct} />
-            </article>
-            <article className='product__page__review'>
-                <CurrentProductReview comments={commentsData} />
-            </article>
+            {
+                !currentProduct
+                    ? <CLoader />
+                    : <>
+                        <article className='product__page__current__product'>
+                            <CurrentProduct product={currentProduct} />
+                        </article>
+                        <article className='product__page__review'>
+                            <CurrentProductReview comments={commentsData} />
+                        </article>
+                    </>
+            }
         </section>
     )
 }
